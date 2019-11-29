63c02a3f89e6cac33c00970da058feab6c286c23
Захар Максименко <Maksimenko_ZA@edu.bstu.ru>
2019-11-29 16:52:52 +0300

    Залил прогу

---
+++ MouseTestFor_8141/MouseTestFor_8141/main.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace MouseTestFor_8141
+{
+    partial class main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
+            this.infoLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.infoLabel.ForeColor = System.Drawing.Color.White;
+            this.infoLabel.Location = new System.Drawing.Point(12, 9);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(571, 184);
+            this.infoLabel.TabIndex = 0;
+            this.infoLabel.Text = resources.GetString("infoLabel.Text");
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(969, 621);
+            this.Controls.Add(this.infoLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MouseTestFor_8141";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label infoLabel;
+    }
+}
+
---
+++ MouseTestFor_8141/MouseTestFor_8141/main.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Media;
+using System.Threading;
+
+namespace MouseTestFor_8141
+{
+    /// <summary>
+    /// Класс основной формы приложения
+    /// </summary>
+    public partial class main : Form
+    {
+        /// <summary>
+        /// Цвет заднего фона
+        /// </summary>
+        private Color bgColor;
+        /// <summary>
+        /// Флаг запуска писка
+        /// </summary>
+        private bool sound;
+        /// <summary>
+        /// Плеер для писка
+        /// </summary>
+        private SoundPlayer player;
+
+        /// <summary>
+        /// Конструктор формы
+        /// </summary>
+        public main()
+        {
+            //Инициаизируем компоненты
+            InitializeComponent();
+            //Инициализируем класс
+            init();
+        }
+
+        /// <summary>
+        /// Инициализируем класс
+        /// </summary>
+        private void init()
+        {
+            //Указываем цвет заднего фона
+            bgColor = Color.Black;
+            //Указываем, что звук не нужен
+            sound = false;
+            //Инициализируем плеер звуком
+            player = new SoundPlayer(Properties.Resources.Windows_Default);
+
+
+            //Добавляем обработчик события нажатия кнопки мыши
+            this.MouseDown += Main_MouseDown;
+            //Добавляем обработчик события отпускания кнопки мыши
+            this.MouseUp += Main_MouseUp;
+            //Добавляем обработчик нажатия кнопки на клавиатуре
+            this.KeyDown += Main_KeyDown;
+        }
+
+        /// <summary>
+        /// Обработчик нажатия кнопки на клавиатуре
+        /// </summary>
+        private void Main_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Если нажат ескейп
+            if (e.KeyCode == Keys.Escape)
+                //Закрываем форму
+                this.Close();
+        }
+
+        /// <summary>
+        /// Обработчик события нажатия кнопки мыши
+        /// </summary>
+        private void Main_MouseDown(object sender, MouseEventArgs e)
+        {
+            //Выбираем значение по нажатой кнопке
+            switch(e.Button)
+            {
+                //Левая кнопка мыши
+                case MouseButtons.Left:
+                    {
+                        bgColor = Color.Red;
+                        break;
+                    }
+                //Правая кнопка мыши
+                case MouseButtons.Right:
+                    {
+                        bgColor = Color.Green;
+                        break;
+                    }
+                //Центральная кнопка мыши
+                case MouseButtons.Middle:
+                    {
+                        bgColor = Color.Blue;
+                        break;
+                    }
+            }
+            //Указываем, что есть нажатая кнопка
+            sound = true;
+            //Обновляем цвет заднего фона и текста подсказки
+            updateBg();
+            //Обновляем издавание звука
+            updateSound();
+        }
+
+        /// <summary>
+        /// Обработчик события отпускания кнопки мыши
+        /// </summary>
+        private void Main_MouseUp(object sender, MouseEventArgs e)
+        {
+            //Ставим дефолтный цвет
+            bgColor = Color.Black;
+            //Указываем, что кнопка была отпущена
+            sound = false;
+            //Обновляем цвет заднего фона и текста подсказки
+            updateBg();
+            //Обновляем издавание звука
+            updateSound();
+        }
+
+        /// <summary>
+        /// Обновляем цвет заднего фона и текста подсказки
+        /// </summary>
+        private void updateBg()
+        {
+            //Ставим цвет заднего фона
+            this.BackColor = bgColor;
+            //Ставим в подсказку инверсный цвет
+            infoLabel.ForeColor = Color.FromArgb(255 - bgColor.R, 255 - bgColor.G, 255 - bgColor.B);
+        }
+
+        /// <summary>
+        /// Обновляем издавание звука
+        /// </summary>
+        private void updateSound()
+        {
+            //Если звук должен идти
+            if (sound)
+                //Продолжаем проигрывание
+                player.PlayLooping();
+            //В противном случае
+            else
+                //Стопаем его
+                player.Stop();
+        }
+    }
+}
